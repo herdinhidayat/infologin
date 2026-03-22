91ce8ef2464b400cf74ecc57f87a17d1ccfca9ca
Herdin Hidayat <bumaberau3@gmail.com>
2026-03-22 19:53:26 +0800

    nambah route dan view

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+<DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Contact Us</title>
 </head>
 <body>
-<h1>Selamat Datang</h1>
+    <h1>Halaman Contact</h1>
 </body>
 </html>
