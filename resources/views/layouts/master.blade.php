6cd947d97036ffe23e8b41c50d59a00b651ecebe
Herdin Hidayat <bumaberau3@gmail.com>
2026-03-28 21:23:14 +0800

    template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,4 @@
-html lang="en">
+<html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -21,7 +21,7 @@ html lang="en">
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+          <a class="nav-link" aria-current="page" href="/">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/about">Tentang</a>
--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.master')
+
+@section('konten')
+    <h1>Selamat Datang di Beranda</h1>
+@endsection
