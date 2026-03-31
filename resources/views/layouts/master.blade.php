df94c6f872566cf2cd21b06cd25f4383151e40b6
Herdin Hidayat <bumaberau3@gmail.com>
2026-03-31 14:25:13 +0800

    banyak perubahan

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,6 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <title>Info Login</title>
   </head>
   <body>
@@ -15,12 +14,8 @@
 <div class="container mt-3">
     {{-- Isi Kontern --}}
     @yield('konten')
-
 </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-        
+@include('layouts.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>      
   </body>
 </html>
--- resources/views/pages/kegiatan.blade.php
+++ resources/views/pages/kegiatan.blade.php
@@ -2,4 +2,36 @@
 
 @section('konten')
     <h1>Kegiatan Kami</h1>
+    <hr>
+    <button type="button" class="btn btn-primary mb-3">Tambah Kegiatan</button>
+    <div class="card-header">
+        Daftar Kegiatan
+    </div>
+
+    <table class="table table-striped table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama Kegiatan</th>
+      <th scope="col">Aksi Kegiatan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Berbagi Kepanti Asuhan</td>
+      <td>Selengkapnya</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Berbagi Kepondok</td>
+      <td>Selengkapnya</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Pelatihan Keterampilan</td>
+      <td>Selengkapnya</td>
+    </tr>
+  </tbody>
+</table>
 @endsection
