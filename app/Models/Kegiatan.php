7e2fd160feb8c0ab2753fbe59d21549f955c20c7
Herdin Hidayat <bumaberau3@gmail.com>
2026-04-09 10:03:14 +0800

    edit

--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kegiatan extends Model
 {
-    proc
+    // inisialisasi tabel produk
+    protected $table = 'tb_kegiatan';
+     // inisialisasi primary key
+    protected $primaryKey = 'id_kegiatan';   
+     // inisialisasi data yang dapat kita isi
+    // protected $fillable = ['nama_kegiatan', 'alamat', 'detail_kegiatan'];
+     // inisialisasi tabel data yang tidak boleh diisi
+    protected $guarded = ['id_kegiatan'];
 }
